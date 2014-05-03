4238a02cf686f1f1863283aaa308707a4dbe6583
Adam Curtis <acurtis@spsu.edu>
2014-05-03 18:56:05 -0400

    greatly simplify DepthSet implementation by inheriting from SortedSet

--- GameEngineConcept/Graphics/DepthSet.cs
+++ GameEngineConcept/Graphics/DepthSet.cs
@@ -12,55 +12,18 @@ namespace GameEngineConcept.Graphics
         }
     }
 
-    public class DepthSet : IDrawable, ICollection<IDrawableDepth>
+    public class DepthSet : SortedSet<IDrawableDepth>, IDrawable
     {
-        SortedSet<IDrawableDepth> depthSet;
+        static DepthComparer comparer = new DepthComparer();
 
-        public bool IsReadOnly { get { return true; } }
-        public int Count { get { return depthSet.Count; } }
-
-        public DepthSet() {
-            depthSet = new SortedSet<IDrawableDepth>(new DepthComparer());
-        }
-
-        public void Add(IDrawableDepth obj)
-        {
-            depthSet.Add(obj);
-        }
-
-        public bool Remove(IDrawableDepth obj)
-        {
-            return depthSet.Remove(obj);
-        }
-
-        public bool Contains(IDrawableDepth obj)
-        {
-            return depthSet.Contains(obj);
-        }
-
-        public void CopyTo(IDrawableDepth[] arr, int index) {
-            depthSet.CopyTo(arr, index);
-        }
-
-        public void Clear()
-        {
-            depthSet.Clear();
-        }
+        public DepthSet() : base(comparer) { }
 
         public void Draw()
         {
-            foreach(var obj in depthSet) 
+            foreach(var obj in this) 
             {
                 obj.Draw();
             }
         }
-
-        public void Resort()
-        {
-            depthSet = new SortedSet<IDrawableDepth>(depthSet);
-        }
-
-        public IEnumerator<IDrawableDepth> GetEnumerator() { return depthSet.GetEnumerator(); }
-        IEnumerator IEnumerable.GetEnumerator() { return depthSet.GetEnumerator(); }
     }
 }
