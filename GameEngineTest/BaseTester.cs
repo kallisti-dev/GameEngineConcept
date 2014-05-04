d1f41f65ce9edfb01ca34aa042079bc44197a03d
Adam Curtis <acurtis@spsu.edu>
2014-05-04 16:47:20 -0400

    first implementation of TestWindow

--- GameEngineTest/BaseTester.cs
+++ GameEngineTest/BaseTester.cs
@@ -6,16 +6,16 @@ namespace GameEngineTest
     public abstract class BaseTester
     {
 
-        public event Action<bool> TestComplete;
+        public event Action<BaseTester, bool> TestComplete;
 
         public void TestSuccess()
         {
-            TestComplete(true);
+            TestComplete(this, true);
         }
 
         public void TestFailure()
         {
-            TestComplete(false);
+            TestComplete(this, false);
         }
 
         public virtual void OnLoad() { }
--- GameEngineTest/TestWindow.cs
+++ GameEngineTest/TestWindow.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Diagnostics;
 using System.Threading.Tasks;
+using OpenTK;
+using OpenTK.Platform;
 
 using GameEngineConcept;
 
@@ -10,11 +11,74 @@ namespace GameEngineTest
 {
     class TestWindow : MainWindow
     {
-        List<ITester> tests;
+        Queue<BaseTester> tests = new Queue<BaseTester>();
+        BaseTester currentTest = null;
+        int testsSucceeded = 0;
+        int testsFailed = 0;
 
-        public void AddTest(ITester test) 
-        { tests.Add(test); }
-        public void AddTest<T>() where T : ITester, new() 
+        public TestWindow() : base() { }
+
+        public void AddTest(BaseTester test) 
+        { tests.Enqueue(test); }
+        
+        public void AddTest<T>() where T : BaseTester, new() 
         { AddTest(new T()); }
+
+        private void TestCompleteHandler(BaseTester sender, bool success) 
+        {
+            Debug.Assert(sender == currentTest);
+
+            if (success)
+            {
+                testsSucceeded++;
+                Console.WriteLine(sender.GetType() + " succeeded.");
+            }
+            else
+            {
+                testsFailed++;
+                Console.WriteLine(sender.GetType() + " failed.");
+            }
+
+            if (tests.Count > 0)
+                StartNextTest();
+            else
+                Console.WriteLine("All tests completed. Total: " + (testsSucceeded + testsFailed) + " Succeess: " + testsSucceeded + " Failed: " + testsFailed);
+        }
+
+        private void StartNextTest()
+        {
+            if (currentTest != null)
+            {
+                currentTest.OnUnload();
+                currentTest.TestComplete -= TestCompleteHandler;
+            }
+            currentTest = tests.Dequeue();
+            currentTest.TestComplete += TestCompleteHandler;
+            currentTest.OnLoad();
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            StartNextTest();
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+           base.OnRenderFrame(e);
+           currentTest.OnRenderFrame(e);
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+            currentTest.OnUpdateFrame(e);
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+            currentTest.OnResize();
+        }
     }
 }
