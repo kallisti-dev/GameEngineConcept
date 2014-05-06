f26e6dab2b9d91fa6f0fa6f2bdcf457ee33de4b8
Adam Curtis <acurtis@spsu.edu>
2014-05-06 11:21:45 -0400

    docs

--- GameEngineConcept/Graphics/Modes/Interfaces.cs
+++ GameEngineConcept/Graphics/Modes/Interfaces.cs
@@ -3,10 +3,16 @@ using OpenTK.Graphics.OpenGL;
 
 namespace GameEngineConcept.Graphics.Modes
 {
+    //an interface for handling different openGL initializations
     public interface IGraphicsMode
     {
+        //when applying graphics modes to an EngineWindow, matrix modes are automatically handled.
+        //the graphics mode must specify its primary matrix mode, or null if none is used.
         MatrixMode? PrimaryMatrixMode { get; }
+
+        //any initialization code for the graphics mode.
         void Initialize();
+        //any uninitialization code for the graphics mode.
         void Uninitialize();
     }
 }
