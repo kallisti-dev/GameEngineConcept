06db5ad31435d2e732bf3dcb116fc55c1d2bc9dc
ExitiumDeus <jselmser@spsu.edu>
2014-05-16 01:55:08 -0400

    timeout
    all commits are to be revised by beemo, the great

--- GameEngineTest/BaseTester.cs
+++ GameEngineTest/BaseTester.cs
@@ -8,6 +8,8 @@ namespace GameEngineTest
     {
 
         public event Action<BaseTester, bool> TestComplete;
+        private int timeoutCounter = 300; //Assuming 60 cycles a second then this is 5 seconds
+        private bool succeedOnTimeout = false;
 
         public void TestSuccess()
         {
@@ -22,7 +24,28 @@ namespace GameEngineTest
         public virtual void OnLoad(TestWindow window) { }
         public virtual void OnUnload(TestWindow window) { }
         public virtual void OnRenderFrame(FrameEventArgs e) { }
-        public virtual void OnUpdateFrame(FrameEventArgs e) { }
+        public virtual void OnUpdateFrame(FrameEventArgs e) 
+        {
+            Timeout();
+        }
         public virtual void OnResize() { }
+        public virtual void Timeout()
+        {
+            Console.Out.WriteLine("Timeout is called" + timeoutCounter);
+            if (timeoutCounter <= 0)
+            {
+                if (SucceedOnTimeout)
+                {
+                    TestSuccess();
+                }
+                else TestFailure();
+            }
+            else timeoutCounter--;
+        }
+        public bool SucceedOnTimeout
+        {
+            get { return succeedOnTimeout; }
+            set { succeedOnTimeout = value; }
+        }
     }
 }
